8fb321d8728face273e74cbaf46a5d1ed8d1668d
jocelynyuan <jocelyn19950627@yahoo.com.tw>
2021-07-24 14:17:37 +0900

    reccomendation

--- GiveMeSomeCredit/app.py
+++ GiveMeSomeCredit/app.py
@@ -11,26 +11,28 @@ def main():
        #'Contacts_Count_12_mon', 'Credit_Limit', 'Total_Revolving_Bal',
        #'Total_Amt_Chng_Q4_Q1', 'Total_Trans_Amt', 'Total_Trans_Ct',
        #'Total_Ct_Chng_Q4_Q1', 'Avg_Utilization_Ratio']
-    Customer_Age = st.text_input('Age as Customer (in Years)', "18")
+    Customer_Age = st.number_input('Age as Customer (in Years)', 62)
     Dependent_count= st.text_input('Number of dependents', "2")
     Education_Level= st.selectbox('Educational Qualification of the account holder', ('Uneducated', 'High School', 'College', 'Unknown', 'Graduate', 'Post-Graduate', 'Doctorate'))
     Income_Category= st.selectbox('Annual Income Category of the account holder', ('Less than $40K', '$40K - $60K', 'Unknown','$60K - $80K', '$80K - $120K', '$120K +'))
-    Months_on_book = st.text_input('Period of relationship with the bank (in Months)', "12")
-    Total_Relationship_Count= st.text_input('Total no. of products held by the customer', "2")
-    Contacts_Count_12_mon= st.text_input('No. of Contacts in the last 12 months', "20")
-    Credit_Limit= st.text_input('Credit Limit on the Credit Card', "10000")
-    Total_Revolving_Bal= st.text_input('Total Revolving Balance on the Credit Card', "20000")
-    Total_Amt_Chng_Q4_Q1= st.text_input('Change in Transaction Amount (Q4 over Q1)', "21000")
-    Total_Trans_Amt= st.text_input('Total Transaction Amount (Last 12 months)', "20")
-    Total_Trans_Ct= st.text_input('Total Transaction Count (Last 12 months)', "1")
-    Total_Ct_Chng_Q4_Q1= st.text_input('Change in Transaction Count (Q4 over Q1)', "2")
-    Avg_Utilization_Ratio= st.text_input('Average Card Utilization Ratio', "0.8")
+    Months_on_book = st.number_input('Period of relationship with the bank (in Months)', 49)
+    Total_Relationship_Count= st.number_input('Total no. of products held by the customer', 2)
+    Contacts_Count_12_mon= st.number_input('No. of Contacts in the last 12 months', 3)
+    Credit_Limit= st.number_input('Credit Limit on the Credit Card', 1438)
+    Total_Revolving_Bal= st.number_input('Total Revolving Balance on the Credit Card', 1436)
+    Total_Amt_Chng_Q4_Q1= st.number_input('Change in Transaction Amount (Q4 over Q1)', 1.047)
+    Total_Trans_Amt= st.number_input('Total Transaction Amount (Last 12 months)', 692)
+    Total_Trans_Ct= st.number_input('Total Transaction Count (Last 12 months)', 1)
+    Total_Ct_Chng_Q4_Q1= st.number_input('Change in Transaction Count (Q4 over Q1)', 2)
+    Avg_Utilization_Ratio= st.number_input('Average Card Utilization Ratio', 0.9994)
     Months_Inactive_12_mon= st.slider('No. of months inactive in the last 12 months',1,12,1)
     Gender = ''
     Marital_Status = ''
     Card_Category = ''
 
-
+    column_name = ['Customer_Age','Gender','Dependent_count','Education_Level','Marital_Status','Income_Category','Card_Category','Months_on_book','Total_Relationship_Count',
+    'Months_Inactive_12_mon','Contacts_Count_12_mon','Credit_Limit','Total_Revolving_Bal','Total_Amt_Chng_Q4_Q1','Total_Trans_Amt',
+    'Total_Trans_Ct','Total_Ct_Chng_Q4_Q1','Avg_Utilization_Ratio']
 
 
 
@@ -38,15 +40,20 @@ def main():
     Months_Inactive_12_mon,Contacts_Count_12_mon,Credit_Limit,Total_Revolving_Bal,Total_Amt_Chng_Q4_Q1,Total_Trans_Amt,
     Total_Trans_Ct,Total_Ct_Chng_Q4_Q1,Avg_Utilization_Ratio]]
 
+
+    model = joblib.load('credit_new_model.joblib')
+
      # and store it in the variable result
     if st.button("Predict"):
-        result_input = prediction_input(input_list)
-        st.success(result_input)
+        result_input = prediction_input(input_list,model)
+        st.dataframe(result_input)
 
-        if result_input == 0:
-            if  st.button("Recommodation"):
-                change, proba, state = recommodation(result_input)
-                st.success('The recommodation is {}'.format(change, proba, state))
+        X = result_input[column_name]
+        y = result_input['Prediction']
+
+        #if y.values == 0:
+        change, proba, state = recommendation(X,model)
+        st.write('The recommendation is {},{},{}'.format(change, proba, state))
 
 
 
--- GiveMeSomeCredit/prediction.py
+++ GiveMeSomeCredit/prediction.py
@@ -1,5 +1,6 @@
 import joblib
 import pandas as pd
+
 def prediction_test():
     df = pd.read_csv('../raw_data/BankChurners.csv')
     df.drop(['Attrition_Flag','CLIENTNUM','Naive_Bayes_Classifier_Attrition_Flag_Card_Category_Contacts_Count_12_mon_Dependent_count_Education_Level_Months_Inactive_12_mon_1','Naive_Bayes_Classifier_Attrition_Flag_Card_Category_Contacts_Count_12_mon_Dependent_count_Education_Level_Months_Inactive_12_mon_2','Avg_Open_To_Buy'],axis=1, inplace=True)
@@ -8,34 +9,38 @@ def prediction_test():
     prediction = model.predict(X_test)
     return prediction
 
-def prediction (test):
+def prediction (test,model):
     df = pd.read_csv(test)
-    model = joblib.load('credit_new_model.joblib')
     prediction = model.predict(df)
     df['Prediction'] = prediction
     return df
 
-def prediction_input(input_list):
+def prediction_input(input_list,model):
     column_name = ['Customer_Age','Gender','Dependent_count','Education_Level','Marital_Status','Income_Category','Card_Category','Months_on_book','Total_Relationship_Count',
     'Months_Inactive_12_mon','Contacts_Count_12_mon','Credit_Limit','Total_Revolving_Bal','Total_Amt_Chng_Q4_Q1','Total_Trans_Amt',
     'Total_Trans_Ct','Total_Ct_Chng_Q4_Q1','Avg_Utilization_Ratio']
     df = pd.DataFrame(data = input_list, columns= column_name)
-    model = joblib.load('credit_new_model.joblib')
+    #model = joblib.load('credit_new_model.joblib')
     prediction = model.predict(df)
-    #df['Prediction'] = prediction
-    return prediction
+    df['Prediction'] = prediction
+    return df
 
 
 
-def recommodation(churning):
-    proba = rf.predict_proba(churning)[0][1]*100
+def recommendation(churning,model):
+    churning = churning.iloc[[0]]
+    print(churning)
+    proba = model.predict_proba(churning)[0][1]*100
     fac = 1
+    state = 1
     while proba < 50:
       fac += 0.01
       X_temp = churning
-      X_temp.Total_Trans_Ct = X_temp.Total_Trans_Ct*fac
-      X_temp.Total_Trans_Amt = X_temp.Total_Trans_Amt*fac
-      proba = rf.predict_proba(X_temp)[0][1]*100
-      state = rf.predict(X_temp)[0]
+      print(type(X_temp.Total_Trans_Ct.values[0]))
+      X_temp.Total_Trans_Ct = X_temp.Total_Trans_Ct.values[0]*fac
+
+      X_temp.Total_Trans_Amt = X_temp.Total_Trans_Amt.values[0]*fac
+      proba = model.predict_proba(X_temp)[0][1]*100
+      state = model.predict(X_temp)[0]
     change = (fac-1)*100
     return change, proba, state
