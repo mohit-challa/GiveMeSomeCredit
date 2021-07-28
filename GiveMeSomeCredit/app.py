7ff57377f47edd3919dcbaef8e0b9f5cca4efb97
jocelynyuan <jocelyn19950627@yahoo.com.tw>
2021-07-28 13:51:31 +0900

    banner added

--- GiveMeSomeCredit/app.py
+++ GiveMeSomeCredit/app.py
@@ -4,13 +4,41 @@ import streamlit as st
 import streamlit.components.v1 as components
 def main():
       # giving the webpage a title
+    model = joblib.load('credit_new_model.joblib')
+    column_name = ['Customer_Age','Gender','Dependent_count','Education_Level','Marital_Status','Income_Category','Card_Category','Months_on_book','Total_Relationship_Count',
+    'Months_Inactive_12_mon','Contacts_Count_12_mon','Credit_Limit','Total_Revolving_Bal','Total_Amt_Chng_Q4_Q1','Total_Trans_Amt',
+    'Total_Trans_Ct','Total_Ct_Chng_Q4_Q1','Avg_Utilization_Ratio']
     st.set_page_config(page_title="Credit card churn prediction", page_icon="", layout="wide")
-    st.title("Credit card churn prediction")
-    st.markdown(""" <style>
+    #st.title("Credit card churn prediction")
+    #st.markdown(""" <style>
         #MainMenu {visibility: hidden;}
-        footer {visibility: hidden;}
-        </style> """, unsafe_allow_html=True)
-    st.subheader("_Please fill the fields below or upload a CSV file with the Customer's information_")
+        #footer {visibility: hidden;}
+        #</style> """, unsafe_allow_html=True)
+    st.image('Banner.png')
+    st.subheader("_Please upload a CSV file with the Customer's information_")
+    uploaded_file = st.file_uploader('')
+    if st.button("Predict using CSV file"):
+      if uploaded_file is not None:
+      #if uploaded_file is not None:
+        result = prediction(uploaded_file,model)
+        n = (result['Prediction'].values == 0).sum()
+        if n > 0:
+            st.error(f'{n} customer(s) will churn')
+            #st.dataframe(result[result.Prediction == 0 ])
+            X = result[result.Prediction == 0 ][column_name]
+            y = result['Prediction']
+            for i in range(X.shape[0]) :
+                change, proba, state = recommendation(X.iloc[[i]],model)
+                st.dataframe(X.iloc[[i]])
+                st.info(
+                    "**To prevent the churn of this customer, please consider the below actions:**\n""\n"
+                    "Encourage the customer to increase Total_Transaction_Amount and Total_Transaction_Count by: {}\n%\n""\n"
+                    "The new probability of a customer not churning is: {}\n%\n"
+                    "*************************************************************"
+                    .format(round(change,2), round(proba,2)))
+        else:
+            st.success('The Customer will not churn')
+    st.subheader("_Please fill the fields below with the Customer's information_")
     col1,col2,col3= st.beta_columns([4,1,4])
     # the following lines create text boxes in which the user can enter
     # the data required to make the prediction
@@ -23,7 +51,6 @@ def main():
     Marital_Status = ''
     Card_Category = ''
     result_input = pd.DataFrame()
-    model = joblib.load('credit_new_model.joblib')
     with col1:
         Customer_Age = st.number_input('Age', min_value=1, max_value=150, value=62, step=1)
         Dependent_count= st.selectbox('Number of dependents',(0,1,2,3,4,5,6,7,8,9))
@@ -49,11 +76,6 @@ def main():
             Total_Trans_Ct,Total_Ct_Chng_Q4_Q1,Avg_Utilization_Ratio]]
             result_input = prediction_input(input_list,model)
 
-
-    column_name = ['Customer_Age','Gender','Dependent_count','Education_Level','Marital_Status','Income_Category','Card_Category','Months_on_book','Total_Relationship_Count',
-    'Months_Inactive_12_mon','Contacts_Count_12_mon','Credit_Limit','Total_Revolving_Bal','Total_Amt_Chng_Q4_Q1','Total_Trans_Amt',
-    'Total_Trans_Ct','Total_Ct_Chng_Q4_Q1','Avg_Utilization_Ratio']
-
      # and store it in the variable result
      # Added if statement for improving UI (The customer will (0 churn) (1 not churn)) Mirko
     if not result_input.empty:
@@ -72,29 +94,6 @@ def main():
                 "Encourage the customer to increase Total_Transaction_Amount and Total_Transaction_Count by: {}\n"" %\n"
                 "\nThe new probability of a customer not churning is: {}\n"" %\n"
                 .format(round(change,2), round(proba,2)))
-
-    uploaded_file = st.file_uploader('Choose a CSV file')
-    print (uploaded_file)
-    if st.button("Predict using CSV file"):
-      if uploaded_file is not None:
-        result = prediction(uploaded_file,model)
-        n = (result['Prediction'].values == 0).sum()
-        if n > 0:
-            st.error(f'{n} customer(s) will churn')
-            #st.dataframe(result[result.Prediction == 0 ])
-            X = result[result.Prediction == 0 ][column_name]
-            y = result['Prediction']
-            for i in range(X.shape[0]) :
-                change, proba, state = recommendation(X.iloc[[i]],model)
-                st.dataframe(X.iloc[[i]])
-                st.info(
-                    "**To prevent the churn of this customer, please consider the below actions:**\n""\n"
-                    "Encourage the customer to increase Total_Transaction_Amount and Total_Transaction_Count by: {}\n%\n""\n"
-                    "The new probability of a customer not churning is: {}\n%\n"
-                    "*************************************************************"
-                    .format(round(change,2), round(proba,2)))
-        else:
-            st.success('The Customer will not churn')
 
         #st.success('The output is {}'.format(result.Prediction))
 if __name__=='__main__':
